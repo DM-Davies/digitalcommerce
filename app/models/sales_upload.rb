abd0aebf678fa3fba7ed154f1f4a62a885f8b596
Daniel Davies <daniel.davies@dubitlimited.com>
2018-09-25 12:33:58 +0100

    Reorder models closer to best practice

--- app/models/sales_upload.rb
+++ app/models/sales_upload.rb
@@ -1,27 +1,17 @@
 class SalesUpload < ApplicationRecord
-
-  mount_uploader :video, VideoUploader
   serialize :video, JSON # If you use SQLite, add this line.
 
-  def order_sales_relationship
-    Order.where(id: SalesUpload.pluck(:order_id))
-    
-
-  end
-
-
-  def ordertits
-    Order.where(:id) && SalesUpload.where(:order_id)
-
-  end
-  #belongs_to :order
+  mount_uploader :video, VideoUploader
 
   belongs_to :user
-#  belongs_to :buyer
-  #has_many :listings
-
-  #has_many :orders
   has_one :order
   has_one :buyer
 
+  def order_sales_relationship
+    Order.where(id: SalesUpload.pluck(:order_id))
+  end
+
+  def ordertits
+    Order.where(:id) && SalesUpload.where(:order_id)
+  end
 end
