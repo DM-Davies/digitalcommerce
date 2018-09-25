51e90ac6f45c2b2b31e89c1100e7f1e525cdfe29
Daniel Davies <daniel.davies@dubitlimited.com>
2018-09-25 12:35:21 +0100

    Change `has_one` to `belongs_to` and remove redundant methods

--- app/models/sales_upload.rb
+++ app/models/sales_upload.rb
@@ -3,15 +3,8 @@ class SalesUpload < ApplicationRecord
 
   mount_uploader :video, VideoUploader
 
+  belongs_to :order
   belongs_to :user
-  has_one :order
+
   has_one :buyer
-
-  def order_sales_relationship
-    Order.where(id: SalesUpload.pluck(:order_id))
-  end
-
-  def ordertits
-    Order.where(:id) && SalesUpload.where(:order_id)
-  end
 end
