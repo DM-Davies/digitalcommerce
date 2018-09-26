216d8d079e128c7ab7d9b4c54d84759d458fe51d
Daniel Davies <daniel.davies@dubitlimited.com>
2018-09-26 14:36:48 +0100

    Seeds file for demo

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Creating Buyer'
+buyer = Buyer.create(name: 'Jonny Buyer',
+                     email: 'j.buyer@example.com',
+                     password: 'password')
+
+puts 'Creating Seller'
+seller = Seller.create(name: 'Timmy Seller',
+                       email: 't.seller@example.com',
+                       password: 'password')
+
+puts 'Creating First Order (Kylo Ren)'
+order = Order.create(buyer: buyer, seller: seller)
+
+puts 'Creating Sales Upload through association'
+order.create_sales_upload!(video: 'https://www.youtube.com/watch?v=CV65tWCZALI')
+
+puts 'Creating Second Order that will not have a Sales Upload'
+Order.create(buyer: buyer, seller: seller)
