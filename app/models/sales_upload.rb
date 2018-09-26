a30233f2079627c798fc949c19591fb4cd4528fd
Daniel Davies <daniel.davies@dubitlimited.com>
2018-09-26 14:35:42 +0100

    Update SalesUpload for demonstration

--- app/models/sales_upload.rb
+++ app/models/sales_upload.rb
@@ -1,27 +1,9 @@
 class SalesUpload < ApplicationRecord
-
-  mount_uploader :video, VideoUploader
   serialize :video, JSON # If you use SQLite, add this line.
 
-  def order_sales_relationship
-    Order.where(id: SalesUpload.pluck(:order_id))
-    
+  mount_uploader :video, VideoUploader
 
-  end
-
-
-  def ordertits
-    Order.where(:id) && SalesUpload.where(:order_id)
-
-  end
-  #belongs_to :order
-
-  belongs_to :user
-#  belongs_to :buyer
-  #has_many :listings
-
-  #has_many :orders
-  has_one :order
-  has_one :buyer
-
+  belongs_to :order
+  has_one :buyer, through: :order
+  has_one :seller, through: :order
 end
